605f9d12e6c0b119504836731bb79b280c806c8f
Breno Morais <breno.lopes.morais@hotmail.com>
2018-02-09 01:41:33 -0200

    +Add class Autor and AutorDao, persistence in database +Configuration list autores page web ok
    by breno morais

--- store/src/main/java/br/com/breno/store/beans/AdminLivrosBean.java
+++ store/src/main/java/br/com/breno/store/beans/AdminLivrosBean.java
@@ -1,11 +1,16 @@
 package br.com.breno.store.beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
 
+import br.com.breno.store.daos.AutorDao;
 import br.com.breno.store.daos.LivroDao;
+import br.com.breno.store.models.Autor;
 import br.com.breno.store.models.Livro;
 
 //CDI
@@ -13,17 +18,39 @@ import br.com.breno.store.models.Livro;
 @RequestScoped
 public class AdminLivrosBean {
 	
-	private Livro livro = new Livro();
+	private Livro livro = new Livro();	
 	
 	@Inject
-	private LivroDao dao;
+	private LivroDao livroDao;
 	
-	@Transactional
-	public void salvar() {		
-		dao.salvar(livro);
-		System.out.println("Livro salvo com sucesso! "+livro);
+	@Inject 
+	private AutorDao autorDao;
+	
+	private List<Integer> autoresId = new ArrayList<>();;
+	
+	public void limparCampos() {
+		this.livro = new Livro();
+		this.autoresId = new ArrayList<>();
 	}
 
+	@Transactional
+	public void salvar() {
+		for(Integer autorId : autoresId) {
+			livro.getAutores().add(new Autor(autorId));
+		}
+		
+		livroDao.salvar(livro);		
+		
+		System.out.println("Livro salvo com sucesso! "+livro+" - Autor: "+autoresId);
+		
+		limparCampos();
+	}
+
+	public List<Autor> getAutores(){
+		return  autorDao.listar();
+		
+	}
+	
 	public Livro getLivro() {
 		return livro;
 	}
@@ -31,5 +58,15 @@ public class AdminLivrosBean {
 	public void setLivro(Livro livro) {
 		this.livro = livro;
 	}
+
+	public List<Integer> getAutoresId() {
+		return autoresId;
+	}
+
+	public void setAutoresId(List<Integer> autoresId) {
+		this.autoresId = autoresId;
+	}
+	
+	
 
 }
---
+++ store/src/main/java/br/com/breno/store/daos/AutorDao.java
@@ -0,0 +1,20 @@
+package br.com.breno.store.daos;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.breno.store.models.Autor;
+
+public class AutorDao {
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	public List<Autor> listar() {		
+		return manager.createQuery("select a from Autor a", Autor.class).getResultList();
+	}	
+	
+
+}
