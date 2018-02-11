9ed527714aa4eaeda28b5ebc0a043e81b73398d3
Breno Morais <breno.lopes.morais@hotmail.com>
2018-02-11 03:39:35 -0200

    Implementing book cover image display on the web page listing. With servelt for transfer of file server for page web.
    by breno morais

--- store/src/main/java/br/com/breno/store/infra/FileSaver.java
+++ store/src/main/java/br/com/breno/store/infra/FileSaver.java
@@ -1,12 +1,20 @@
 package br.com.breno.store.infra;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.Path;
 
 import javax.servlet.http.Part;
 
 public class FileSaver {
 	
-	private static final String SERVER_PATH = "/store";
+	public static final String SERVER_PATH = "/store";
 	
 	public String write(Part arquivo, String path) {
 		
@@ -18,6 +26,25 @@ public class FileSaver {
 		}catch(IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	public static void transfer(Path source, OutputStream outputStream) {
+	    try {
+	        FileInputStream input = new FileInputStream(source.toFile());	        
+	        try( ReadableByteChannel inputChannel = Channels.newChannel(input);
+	                WritableByteChannel outputChannel = Channels.newChannel(outputStream)) {
+	            ByteBuffer buffer = ByteBuffer.allocateDirect(1024 * 10);
+	            while(inputChannel.read(buffer) != -1) {
+	            	buffer.flip();
+	            	outputChannel.write(buffer);
+	            	buffer.clear();	            	
+	            }
+	        } catch (IOException e) {
+	            throw new RuntimeException(e); 
+	        }
+	    } catch (FileNotFoundException e) {
+	        throw new RuntimeException(e); 
+	    }
 	}
 
 }
