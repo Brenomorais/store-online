35cc0968f81339d053f6148e936efaaa0eccb6de
Breno Morais <breno.lopes.morais@hotmail.com>
2018-02-21 17:45:01 -0300

    Create page web login, configuration server jaas injboos with roles for authentic login in application. by breno morais

---
+++ store/src/main/resources/files-database/script.sql
@@ -0,0 +1,4 @@
+-- user admin
+insert into systemuser(login,senha)value('admin','pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=');
+insert into SystemRole value('ADMIN');
+insert into systemuser_systemrole value(1,'ADMIN');
